39d0dfd809e10e6387317405b4bd9f2ec34d564a
unknown <coderhimanshu09@gmail.com>
2024-08-03 12:16:51 +0530

    Changes in models

--- Customer/models.py
+++ Customer/models.py
@@ -8,39 +8,43 @@ from Warehouse.models import Product
 
 class ShippingAddress(TimeStamp):
     ADDRESS_CHOICES = [
-        ("home", "Home"),
-        ("work", "Work"),
-        ("hotel", "Hotel"),
-        ("other", "Other"),
+        ("Home", "Home"),
+        ("Work", "Work"),
+        ("Hotel", "Hotel"),
+        ("Other", "Other"),
     ]
+
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name=_("customer"))
     customer_name = models.CharField(max_length=200, verbose_name=_("customer name"))
     customer_phone = models.CharField(
-        verbose_name=_("customer mobile number"),
         max_length=13,
         validators=[mobile_validator],
+        verbose_name=_("customer mobile number"),
         help_text=_("Alphabets and special characters are not allowed."),
     )
-    address_type = models.CharField(verbose_name=_("address type"), max_length=10, choices=ADDRESS_CHOICES,
-                                    default="home")
+    address_type = models.CharField(
+        max_length=10,
+        choices=ADDRESS_CHOICES,
+        default="home",
+        verbose_name=_("address type"),
+    )
     building_name = models.CharField(max_length=255, verbose_name=_("Flat/House No./Building Name"))
     floor = models.CharField(max_length=20, verbose_name=_("Floor"), blank=True, null=True)
     landmark = models.CharField(max_length=255, verbose_name=_("Nearby Landmark"), blank=True, null=True)
-    latitude = models.DecimalField(verbose_name=_("Latitude"), max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(verbose_name=_("Longitude"), max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, verbose_name=_("Latitude"))
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, verbose_name=_("Longitude"))
     full_address = models.TextField(verbose_name=_("full address"))
 
     class Meta:
-        verbose_name = "Shipping Address"
-        verbose_name_plural = "Shipping Address"
+        verbose_name = _("Shipping Address")
+        verbose_name_plural = _("Shipping Addresses")
 
     def __str__(self):
         return f"{self.customer_name} | {self.address_type}"
 
 
 class Favorite(TimeStamp):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="favorites",
-                                 verbose_name=_("customer"))
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="favorites", verbose_name=_("customer"))
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="favorites", verbose_name=_("product"))
 
     class Meta:
@@ -62,14 +66,13 @@ class Cart(models.Model):
         return f"Cart for {self.customer.name} | {self.customer.phone}"
 
 
-# CartItem Model
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, related_name="cart_items", on_delete=models.CASCADE, verbose_name=_("cart items"))
+    cart = models.ForeignKey(Cart, related_name="cart_items", on_delete=models.CASCADE, verbose_name=_("cart"))
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_("product"))
     quantity = models.PositiveIntegerField(default=1, verbose_name=_("quantity"))
 
     class Meta:
-        verbose_name = _("Cart")
+        verbose_name = _("Cart Item")
         verbose_name_plural = _("Cart Items")
 
     def __str__(self):
@@ -77,24 +80,23 @@ class CartItem(models.Model):
 
 
 class Order(models.Model):
-    STATUS_CHOICES = (
-        ("pending", "Pending"),
-        ("processing", "Processing"),
-        ("completed", "Completed"),
-        ("rejected", "Rejected"),
-    )
+    STATUS_CHOICES = [
+        ("Pending", "Pending"),
+        ("Processing", "Processing"),
+        ("Completed", "Completed"),
+        ("Canceled", "Canceled"),
+    ]
 
-    PAYMENT_METHOD = (
-        ("online", "Online"),
-        ("cod", "Cash on Delivery"),
-    )
-    order_number = models.CharField(max_length=15, verbose_name=_("Order Number"))
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="order", verbose_name=_("customer"))
+    PAYMENT_METHOD = [
+        ("Online", "Online"),
+        ("COD", "Cash on Delivery"),
+    ]
+
+    order_number = models.CharField(max_length=15, verbose_name=_("Order Number"), unique=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="orders", verbose_name=_("customer"))
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("total amount"))
-    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.SET_NULL, null=True, blank=True,
-                                         verbose_name=_("shipping address"))
-    order_status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="pending",
-                                    verbose_name=_("order status"))
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_("shipping address"))
+    order_status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="Pending", verbose_name=_("order status"))
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD, verbose_name=_("payment method"))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("created date"))
 
@@ -104,12 +106,11 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.order_number:
-            last_object = Order.objects.order_by("-id").first()
-            if last_object and last_object.order_number:
-                last_order_number = last_object.order_number
-                order_number_suffix = last_order_number[-10:]
-                new_suffix = str(int(order_number_suffix) + 1).zfill(10)
-                self.order_number = f"ORD_{new_suffix}"
+            last_order = Order.objects.order_by("-id").first()
+            if last_order:
+                last_order_number = int(last_order.order_number.split('_')[1])
+                new_order_number = f"ORD_{str(last_order_number + 1).zfill(10)}"
+                self.order_number = new_order_number
             else:
                 self.order_number = "ORD_0000000001"
         super().save(*args, **kwargs)
@@ -118,14 +119,12 @@ class Order(models.Model):
         return f"Order #{self.order_number}"
 
 
-# OrderItem Model
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items", verbose_name=_("order"))
-    warehouse = models.ForeignKey(WareHouse, on_delete=models.CASCADE, related_name="order_items",
-                                  verbose_name=_("warehouse"))
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_("Product"))
+    warehouse = models.ForeignKey(WareHouse, on_delete=models.CASCADE, related_name="order_items", verbose_name=_("warehouse"))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_("product"))
     quantity = models.PositiveIntegerField(default=1, verbose_name=_("quantity"))
-    item_price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name=_("item price"))
+    item_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("item price"))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("created date"))
 
     class Meta:
@@ -136,26 +135,24 @@ class OrderItem(models.Model):
         return f"{self.quantity} x {self.product.title} in order {self.order.order_number}"
 
 
-# Payment Model
 class Payment(models.Model):
-    PAYMENT_CHOICES = (
-        ("online", "Online"),
-        ("cod", "Cash on Delivery"),
-    )
+    PAYMENT_CHOICES = [
+        ("Online", "Online"),
+        ("COD", "Cash on Delivery"),
+    ]
 
-    PAYMENT_STATUS = (
+    PAYMENT_STATUS = [
         ("Pending", "Pending"),
         ("Completed", "Completed"),
-        ("Cancel", "Cancel"),
-    )
+        ("Canceled", "Canceled"),
+    ]
 
-    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payment",verbose_name=_("customer"))
-    order = models.OneToOneField(Order, on_delete=models.CASCADE,verbose_name=_("order"))
-    payment_method = models.CharField(max_length=255, choices=PAYMENT_CHOICES, verbose_name=_("Payment method"))
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments", verbose_name=_("customer"))
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, verbose_name=_("order"))
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_CHOICES, verbose_name=_("payment method"))
     razorpay_order_id = models.CharField(max_length=200, blank=True, null=True, verbose_name=_("Razorpay Order ID"))
     razorpay_payment_id = models.CharField(max_length=200, blank=True, null=True, verbose_name=_("Razorpay Payment ID"))
     razorpay_signature = models.CharField(max_length=500, blank=True, null=True, verbose_name=_("Razorpay Signature"))
-    razorpay_payment_status = models.CharField(max_length=200, blank=True, null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("amount"))
     payment_status = models.CharField(choices=PAYMENT_STATUS, max_length=50, verbose_name=_("payment status"))
     payment_date = models.DateTimeField(auto_now_add=True, verbose_name=_("payment date"))
@@ -165,4 +162,4 @@ class Payment(models.Model):
         verbose_name_plural = _("Payments")
 
     def __str__(self):
-        return f" Order - {self.order.order_number}"
+        return f"Payment for Order #{self.order.order_number}"
--- Warehouse/models.py
+++ Warehouse/models.py
@@ -84,7 +84,7 @@ class SubCategory(TimeStamp):
 
 class Product(SEO, TimeStamp):
     warehouse = models.ForeignKey(WareHouse, on_delete=models.CASCADE, verbose_name=_("warehouse"))
-    sku_no = models.CharField(verbose_name=_("sku no."), max_length=20)
+    sku_no = models.CharField(verbose_name=_("sku no."), max_length=20, unique=True)
     title = models.CharField(max_length=100, verbose_name=_("product title"))
     size_unit = models.ForeignKey(Unit, on_delete=models.CASCADE, verbose_name=_("size unit"))
     size = models.DecimalField(max_digits=5, decimal_places=2, default=0, verbose_name=_("size"))
@@ -93,31 +93,31 @@ class Product(SEO, TimeStamp):
 
     # Image fields
     image1 = ProcessedImageField(
-        upload_to=image_with_path(path="product/images"),
+        upload_to="product/images",
         format="WEBP",
         options={"quality": 50}, verbose_name=_("Image 1"),
         blank=True, null=True
     )
     image2 = ProcessedImageField(
-        upload_to=image_with_path(path="product/images"),
+        upload_to="product/images",
         format="WEBP",
         options={"quality": 50}, verbose_name=_("Image 2"),
         blank=True, null=True
     )
     image3 = ProcessedImageField(
-        upload_to=image_with_path(path="product/images"),
+        upload_to="product/images",
         format="WEBP",
         options={"quality": 50}, verbose_name=_("Image 3"),
         blank=True, null=True
     )
     image4 = ProcessedImageField(
-        upload_to=image_with_path(path="product/images"),
+        upload_to="product/images",
         format="WEBP",
         options={"quality": 50}, verbose_name=_("Image 4"),
         blank=True, null=True
     )
     image5 = ProcessedImageField(
-        upload_to=image_with_path(path="product/images"),
+        upload_to="product/images",
         format="WEBP",
         options={"quality": 50}, verbose_name=_("Image 5"),
         blank=True, null=True
@@ -125,28 +125,36 @@ class Product(SEO, TimeStamp):
 
     # Attribute fields
     attribute_key1 = models.CharField(verbose_name=_("attribute key 1"), max_length=100, blank=True, null=True)
-    attribute_value1 = models.TextField(verbose_name=_("attribute value 1"), max_length=100, blank=True, null=True)
+    attribute_value1 = CKEditor5Field(verbose_name=_("attribute value 1"), config_name="extends", max_length=100,
+                                      blank=True, null=True)
     attribute_key2 = models.CharField(verbose_name=_("attribute key 2"), max_length=100, blank=True, null=True)
-    attribute_value2 = models.TextField(verbose_name=_("attribute value 2"), max_length=100, blank=True, null=True)
+    attribute_value2 = CKEditor5Field(verbose_name=_("attribute value 2"), config_name="extends", max_length=100,
+                                      blank=True, null=True)
     attribute_key3 = models.CharField(verbose_name=_("attribute key 3"), max_length=100, blank=True, null=True)
-    attribute_value3 = models.TextField(verbose_name=_("attribute value 3"), max_length=100, blank=True, null=True)
+    attribute_value3 = CKEditor5Field(verbose_name=_("attribute value 3"), config_name="extends", max_length=100,
+                                      blank=True, null=True)
     attribute_key4 = models.CharField(verbose_name=_("attribute key 4"), max_length=100, blank=True, null=True)
-    attribute_value4 = models.TextField(verbose_name=_("attribute value 4"), max_length=100, blank=True, null=True)
+    attribute_value4 = CKEditor5Field(verbose_name=_("attribute value 4"), config_name="extends", max_length=100,
+                                      blank=True, null=True)
     attribute_key5 = models.CharField(verbose_name=_("attribute key 5"), max_length=100, blank=True, null=True)
-    attribute_value5 = models.TextField(verbose_name=_("attribute value 5"), max_length=100, blank=True, null=True)
+    attribute_value5 = CKEditor5Field(verbose_name=_("attribute value 5"), config_name="extends", max_length=100,
+                                      blank=True, null=True)
     attribute_key6 = models.CharField(verbose_name=_("attribute key 6"), max_length=100, blank=True, null=True)
-    attribute_value6 = models.TextField(verbose_name=_("attribute value 6"), max_length=100, blank=True, null=True)
+    attribute_value6 = CKEditor5Field(verbose_name=_("attribute value 6"), config_name="extends", max_length=100,
+                                      blank=True, null=True)
     attribute_key7 = models.CharField(verbose_name=_("attribute key 7"), max_length=100, blank=True, null=True)
-    attribute_value7 = models.TextField(verbose_name=_("attribute value 7"), max_length=100, blank=True, null=True)
+    attribute_value7 = CKEditor5Field(verbose_name=_("attribute value 7"), config_name="extends", max_length=100,
+                                      blank=True, null=True)
     attribute_key8 = models.CharField(verbose_name=_("attribute key 8"), max_length=100, blank=True, null=True)
-    attribute_value8 = models.TextField(verbose_name=_("attribute value 8"), max_length=100, blank=True, null=True)
+    attribute_value8 = CKEditor5Field(verbose_name=_("attribute value 8"), config_name="extends", max_length=100,
+                                      blank=True, null=True)
 
     # Other fields
     country_origin = models.ForeignKey(Country, on_delete=models.SET_NULL, blank=True, null=True,
                                        verbose_name=_("country of origin"))
     packaging_type = models.ForeignKey(PackagingType, max_length=50, on_delete=models.CASCADE)
     description = CKEditor5Field(verbose_name=_("description"), help_text=_("Enter Detailed Description of Product"),
-                                  config_name="extends")
+                                 config_name="extends")
     cgst = models.ForeignKey(Tax, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_("CGST"),
                              related_name="cgst_tax")
     sgst = models.ForeignKey(Tax, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_("SGST"),
@@ -167,9 +175,30 @@ class Product(SEO, TimeStamp):
         return f"{self.title} - {self.price} Rs."
 
     def save(self, *args, **kwargs):
-        if not any([self.image1, self.image2, self.image3, self.image4, self.image5]):
-            self.is_active = False
+        # Generate SKU number if it doesn't exist
+        if not self.sku_no:
+            last_product = Product.objects.order_by('id').last()
+            if last_product:
+                last_id = int(last_product.sku_no.split('-')[-1]) if last_product.sku_no else 0
+                self.sku_no = f"SKU{last_id + 1:09d}"  # Format SKU as "SKU-00000001", "SKU-00000002", etc.
+            else:
+                self.sku_no = "SKU000000001"  # Starting SKU
 
+        # Rename images based on SKU
+        if self.image1:
+            self.image1.name = f"product/images/{self.sku_no}_1.webp"
+        if self.image2:
+            self.image2.name = f"product/images/{self.sku_no}_2.webp"
+        if self.image3:
+            self.image3.name = f"product/images/{self.sku_no}_3.webp"
+        if self.image4:
+            self.image4.name = f"product/images/{self.sku_no}_4.webp"
+        if self.image5:
+            self.image5.name = f"product/images/{self.sku_no}_5.webp"
+
+        # Update availability based on stock quantity
         self.is_available = self.stock_quantity > 0
+        self.is_active = self.is_active if any(
+            [self.image1, self.image2, self.image3, self.image4, self.image5]) else False
 
         super().save(*args, **kwargs)
